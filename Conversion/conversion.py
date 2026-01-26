e14fa08129c434f7fbe0c52ccfd2ec34190b6729
Billy Franscois <BillyFranscois@outlook.com>
2026-01-26 11:16:22 +0700

    Update Conversion

--- Conversion/conversion.py
+++ Conversion/conversion.py
@@ -1,7 +1,15 @@
 feet = input("Enter value in feet: ")
 
 inches = float(feet) * 12
+
 print(f"{feet} feet is equal to {inches} inches")
 
 meters = float(feet) * 0.3048
+
 print(f"{feet} feet is equal to {meters} meters")
+
+inch = input("Enter value in inches: ")
+
+feet = float(inch) / 12
+
+print(f"{inch} inches is equal to {feet} feet")
--- Conversion/exp.py
+++ Conversion/exp.py
@@ -1,2 +1,5 @@
 def feetToMeters(feet):
     return feet * 0.3048
+
+def inchToFeet(inch):
+    return inch / 12.0
